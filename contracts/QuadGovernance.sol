d1fd878a4435a75ae73bfd09c82bf1918ca98cc1
teddy-clapp <teddy@quadrata.io>
2022-02-07 15:37:42 -0800

    QSP-10

--- contracts/QuadGovernance.sol
+++ contracts/QuadGovernance.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
