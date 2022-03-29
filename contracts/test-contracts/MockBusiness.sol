0365a605ae7c0896157231f7d97389152d642139
teddy-clapp <teddy@quadrata.io>
2022-03-29 11:49:03 -0700

    remove log

--- contracts/test-contracts/MockBusiness.sol
+++ contracts/test-contracts/MockBusiness.sol
@@ -19,7 +19,6 @@ contract MockBusiness {
     // Must be implemented otherwise tx will revert.
     // See here: https://eips.ethereum.org/EIPS/eip-1155
     function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4) {
-        console.log("Contract Recieved Quadrata Passport NFT");
         return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
     }
 
