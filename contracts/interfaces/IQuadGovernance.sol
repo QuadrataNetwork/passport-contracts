b585d9c2d7be584f7e34fbbc04034d41dafdf4da
teddy-clapp <teddy@quadrata.io>
2022-04-06 13:25:33 -0700

    cleaner interfaces

--- contracts/interfaces/IQuadGovernance.sol
+++ contracts/interfaces/IQuadGovernance.sol
@@ -1,79 +1,77 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+interface IQuadGovernance {
+    function setTreasury(address _treasury) external;
 
-abstract contract IQuadGovernance is AccessControlUpgradeable, UUPSUpgradeable {
-    function setTreasury(address _treasury) external virtual;
+    function setPassportContractAddress(address _passportAddr) external;
 
-    function setPassportContractAddress(address _passportAddr) external virtual;
+    function updateGovernanceInPassport(address _newGovernance) external;
 
-    function updateGovernanceInPassport(address _newGovernance) external virtual;
+    function setPassportVersion(uint256 _version) external;
 
-    function setPassportVersion(uint256 _version) external virtual;
+    function setMintPrice(uint256 _mintPrice) external;
 
-    function setMintPrice(uint256 _mintPrice) external virtual;
+    function setEligibleTokenId(uint256 _tokenId, bool _eligibleStatus) external;
 
-    function setEligibleTokenId(uint256 _tokenId, bool _eligibleStatus) external virtual;
+    function setEligibleAttribute(bytes32 _attribute, bool _eligibleStatus) external;
 
-    function setEligibleAttribute(bytes32 _attribute, bool _eligibleStatus) external virtual;
+    function setEligibleAttributeByDID(bytes32 _attribute, bool _eligibleStatus) external;
 
-    function setEligibleAttributeByDID(bytes32 _attribute, bool _eligibleStatus) external virtual;
+    function setAttributePrice(bytes32 _attribute, uint256 _price) external;
 
-    function setAttributePrice(bytes32 _attribute, uint256 _price) external virtual;
+    function setBusinessAttributePrice(bytes32 _attribute, uint256 _price) external;
 
-    function setBusinessAttributePrice(bytes32 _attribute, uint256 _price) external virtual;
+    function setAttributeMintPrice(bytes32 _attribute, uint256 _price) external;
 
-    function setAttributeMintPrice(bytes32 _attribute, uint256 _price) external virtual;
+     function setOracle(address _oracleAddr) external;
 
-     function setOracle(address _oracleAddr) external virtual;
+     function setRevSplitIssuer(uint256 _split) external;
 
-     function setRevSplitIssuer(uint256 _split) external virtual;
+     function addIssuer(address _issuer, address _treasury) external;
 
-     function addIssuer(address _issuer, address _treasury) external virtual;
-
-     function deleteIssuer(address _issuer) external virtual;
+     function deleteIssuer(address _issuer) external;
 
      function allowTokenPayment(
         address _tokenAddr,
         bool _isAllowed
-    ) external virtual;
+    ) external;
 
-    function getSupportedAttributesLength() external virtual view returns(uint256);
+    function getSupportedAttributesLength() external view returns(uint256);
 
-    function getPrice(address _tokenAddr) external virtual view returns (uint);
+    function getPrice(address _tokenAddr) external view returns (uint);
 
-    function getPriceETH() external virtual view returns (uint);
+    function getPriceETH() external view returns (uint);
 
-    function getIssuersLength() external virtual view returns (uint256);
+    function getIssuersLength() external view returns (uint256);
 
-    function mintPrice() external virtual view returns (uint256);
+    function mintPrice() external view returns (uint256);
 
-    function eligibleTokenId(uint256) external virtual view returns(bool);
+    function eligibleTokenId(uint256) external view returns(bool);
 
-    function issuersTreasury(address) external virtual view returns (address);
+    function issuersTreasury(address) external view returns (address);
 
-    function mintPricePerAttribute(bytes32) external virtual view returns(uint256);
+    function mintPricePerAttribute(bytes32) external view returns(uint256);
 
-    function eligibleAttributes(bytes32) external virtual view returns(bool);
+    function eligibleAttributes(bytes32) external view returns(bool);
 
-    function eligibleAttributesByDID(bytes32) external virtual view returns(bool);
+    function eligibleAttributesByDID(bytes32) external view returns(bool);
 
-    function supportedAttributes(uint256) external virtual view returns(bytes32);
+    function supportedAttributes(uint256) external view returns(bytes32);
 
-    function issuers(uint256) external virtual view returns(address);
+    function issuers(uint256) external view returns(address);
 
-    function pricePerAttribute(bytes32) external virtual view returns(uint256);
+    function pricePerAttribute(bytes32) external view returns(uint256);
 
-    function pricePerBusinessAttribute(bytes32) external virtual view returns(uint256);
-
-
-    function revSplitIssuer() external virtual view returns (uint256);
+    function pricePerBusinessAttribute(bytes32) external view returns(uint256);
 
 
-    function treasury() external virtual view returns (address);
+    function revSplitIssuer() external view returns (uint256);
+
+
+    function treasury() external view returns (address);
+
+    function hasRole(bytes32, address) external view returns(bool);
 
 
 }
