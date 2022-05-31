09d20306fd891939b8f8f087db8c384daf3bee5d
teddy-clapp <teddy@quadrata.io>
2022-05-31 16:47:45 -0700

    Update test_mint.ts

--- test/QuadPassport/test_mint.ts
+++ test/QuadPassport/test_mint.ts
@@ -1259,7 +1259,7 @@ describe("QuadPassport", async () => {
 
 
     });
-    it("success - mint passport to contract", async () => {
+    it("success - mint a business passport for a smart contract owned account", async () => {
 
       const newIsBusiness = id("TRUE")
 
@@ -1285,6 +1285,41 @@ describe("QuadPassport", async () => {
       const promise = passport
         .connect(minterA)
         .mintPassport([mockBusiness.address, TOKEN_ID, did, aml, country, newIsBusiness, issuedAt], sig, '0x00', {
+          value: MINT_PRICE,
+        });
+
+      await promise;
+      expect(await passport.balanceOf(mockBusiness.address, 1)).equals(1);
+      await expect(passport.withdrawETH(protocolTreasury)).to.be.revertedWith('NOT_ENOUGH_BALANCE');
+      const response = await passport.callStatic.withdrawETH(issuerTreasury.address);
+      expect(response).to.equals(MINT_PRICE);
+    });
+
+    it("success - mint a business passport for an EAO", async () => {
+
+      const newIsBusiness = id("TRUE")
+
+      const sig = await signMint(
+        issuer,
+        minterA,
+        TOKEN_ID,
+        did,
+        aml,
+        country,
+        newIsBusiness,
+        issuedAt
+      );
+
+      const sigAccount = await signMessage(minterA, minterA.address);
+
+      expect(await passport.balanceOf(minterA.address, 1)).equals(0);
+      const protocolTreasury = (await governance.config()).treasury;
+      await expect(passport.withdrawETH(protocolTreasury)).to.be.revertedWith('NOT_ENOUGH_BALANCE');
+      await expect(passport.withdrawETH(issuerTreasury.address)).to.not.be.revertedWith('NOT_ENOUGH_BALANCE');
+
+      const promise = passport
+        .connect(minterA)
+        .mintPassport([minterA.address, TOKEN_ID, did, aml, country, newIsBusiness, issuedAt], sig, sigAccount, {
           value: MINT_PRICE,
         });
 
