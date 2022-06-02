b66f510ed0a74dd7238dd1a4f6fbfe6130816f35
teddy-clapp <teddy@quadrata.io>
2022-06-02 13:35:40 -0700

    Update BadMinter.sol

--- contracts/tests/BadMinter.sol
+++ contracts/tests/BadMinter.sol
@@ -7,6 +7,7 @@ import "../storage/QuadPassportStore.sol";
 
 contract BadMinter {
     constructor(address _passport, QuadPassportStore.MintConfig memory _config, bytes memory _sigIssuer, bytes memory _sigAccount) payable {
+        // The sender (ie BadMinter) will have a code length of 0 since it's called via constructor
         IQuadPassport(_passport).mintPassport{value: msg.value}(_config, _sigIssuer, _sigAccount);
     }
 }
