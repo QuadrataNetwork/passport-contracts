ca5fd4e3570a65f90b619b5ee6efd761180bd8b4
teddy-clapp <teddy@quadrata.io>
2022-06-03 01:06:08 -0700

    Update QuadMetaPassport.sol

--- contracts/QuadMetaPassport.sol
+++ contracts/QuadMetaPassport.sol
@@ -12,11 +12,16 @@ contract QuadMetaPassport is UUPSUpgradeable, ERC2771Context {
     IQuadGovernance public governance;
     IQuadPassport public passport;
 
+    // Biconomy's Trusted Forwarder: 0x84a0856b038eaAd1cC7E297cF34A7e72685A8693
+    // Full list for each network found here: https://docs.biconomy.io/misc/contract-addresses
     constructor(address _trustedForwarder, address _governance, address _passport) ERC2771Context(_trustedForwarder) {
         governance = IQuadGovernance(_governance);
         passport = IQuadPassport(_passport);
     }
 
+    /// Note: functions can only be 'meta-wrapped' if they don't use msg.sender
+    /// If the desired function contains msg.sender, then QuadPassport must be upgraded
+    /// with additional functions to support this wrapping.
     function metaMintPassport(
         IQuadPassport.MintConfig calldata config,
         bytes calldata _sigIssuer,
@@ -24,7 +29,6 @@ contract QuadMetaPassport is UUPSUpgradeable, ERC2771Context {
     ) external payable {
         passport.mintPassport(config, _sigIssuer, _sigAccount);
     }
-
 
     function _authorizeUpgrade(address) internal view override {
         require(governance.hasRole(keccak256("GOVERNANCE_ROLE"), _msgSender()), "INVALID_ADMIN");
