723cd262b089a766ec6912212e7dfdf8e34b928a
teddy-clapp <teddy@quadrata.io>
2022-06-03 13:51:45 -0700

    Update test_access_passport.ts

--- test/QuadPassport/test_access_passport.ts
+++ test/QuadPassport/test_access_passport.ts
@@ -85,25 +85,127 @@ describe("READER_ROLE Privileges", async () => {
         await usdc.transfer(minterB.address, parseUnits("1000", 6));
     });
 
-    it("success - a READER_ROLE may query attributes", async () => {
-        const didResponse = await passport.connect(dataChecker).attributes(minterA.address, id("DID"), issuer.address);
-        const countryResponse = await passport.connect(dataChecker).attributes(minterA.address, id("COUNTRY"), issuer.address);
-        const isBusinessResponse = await passport.connect(dataChecker).attributes(minterA.address, id("IS_BUSINESS"), issuer.address);
+    describe("attributes", async () => {
+        it("success - a READER_ROLE may query attributes", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
 
-        expect(didResponse.value).equals(did);
-        expect(countryResponse.value).equals(country);
-        expect(isBusinessResponse.value).equals(isBusiness);
+            const didResponse = await passport.connect(dataChecker).attributes(minterA.address, id("DID"), issuer.address);
+            const countryResponse = await passport.connect(dataChecker).attributes(minterA.address, id("COUNTRY"), issuer.address);
+            const isBusinessResponse = await passport.connect(dataChecker).attributes(minterA.address, id("IS_BUSINESS"), issuer.address);
 
-    })
+            expect(didResponse.value).equals(did);
+            expect(countryResponse.value).equals(country);
+            expect(isBusinessResponse.value).equals(isBusiness);
 
-    it("fail - an ordinary user may not query attributes", async () => {
-        await governance.connect(admin).revokeRole(id("READER_ROLE"), dataChecker.address);
+        })
 
-        expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(false);
+        it("fail - a READER_ROLE may gets null attributes if an issuer is deleted", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
 
-        await expect(passport.connect(dataChecker).attributes(minterA.address, id("DID"), issuer.address)).to.be.revertedWith("INVALID_READER");
-        await expect(passport.connect(dataChecker).attributes(minterA.address, id("COUNTRY"), issuer.address)).to.be.revertedWith("INVALID_READER");
-        await expect(passport.connect(dataChecker).attributes(minterA.address, id("IS_BUSINESS"), issuer.address)).to.be.revertedWith("INVALID_READER");
+            await expect(governance.connect(admin).deleteIssuer(issuer.address))
+                .to.emit(governance, 'IssuerDeleted')
+                .withArgs(issuer.address);
 
-    })
+            const didResponse = await passport.connect(dataChecker).attributes(minterA.address, id("DID"), issuer.address);
+            const countryResponse = await passport.connect(dataChecker).attributes(minterA.address, id("COUNTRY"), issuer.address);
+            const isBusinessResponse = await passport.connect(dataChecker).attributes(minterA.address, id("IS_BUSINESS"), issuer.address);
+
+            expect(didResponse.value).equals(hexZeroPad('0x00', 32));
+            expect(countryResponse.value).equals(hexZeroPad('0x00', 32));
+            expect(isBusinessResponse.value).equals(hexZeroPad('0x00', 32));
+
+        });
+
+        it("fail - a READER_ROLE gets null attributes if an issuer is disabled", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
+
+            await expect(governance.connect(admin).setIssuerStatus(issuer.address, 1))
+                .to.emit(governance, 'IssuerStatusChanged')
+                .withArgs(issuer.address, 0, 1);
+
+            const didResponse = await passport.connect(dataChecker).attributes(minterA.address, id("DID"), issuer.address);
+            const countryResponse = await passport.connect(dataChecker).attributes(minterA.address, id("COUNTRY"), issuer.address);
+            const isBusinessResponse = await passport.connect(dataChecker).attributes(minterA.address, id("IS_BUSINESS"), issuer.address);
+
+            expect(didResponse.value).equals(hexZeroPad('0x00', 32));
+            expect(countryResponse.value).equals(hexZeroPad('0x00', 32));
+            expect(isBusinessResponse.value).equals(hexZeroPad('0x00', 32));
+
+        });
+
+        it("fail - a user without READER_ROLE may not query attributes", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
+            await governance.connect(admin).revokeRole(id("READER_ROLE"), dataChecker.address);
+
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(false);
+
+            await expect(passport.connect(dataChecker).attributes(minterA.address, id("DID"), issuer.address)).to.be.revertedWith("INVALID_READER");
+            await expect(passport.connect(dataChecker).attributes(minterA.address, id("COUNTRY"), issuer.address)).to.be.revertedWith("INVALID_READER");
+            await expect(passport.connect(dataChecker).attributes(minterA.address, id("IS_BUSINESS"), issuer.address)).to.be.revertedWith("INVALID_READER");
+
+        });
+
+        it("fail - cannot access DID level attributes", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
+            expect((await passport.connect(dataChecker).attributes(minterA.address, id("AML"), issuer.address)).value).equals(hexZeroPad('0x00', 32));
+        });
+    });
+
+    describe("attributesByDID", async () => {
+        it("success - a READER_ROLE may query attributes", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
+
+            const amlResponse = await passport.connect(dataChecker).attributesByDID(did, id("AML"), issuer.address);
+
+            expect(amlResponse.value).equals(aml);
+
+        })
+
+        it("fail - a READER_ROLE may gets null attributes if an issuer is deleted", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
+
+            await expect(governance.connect(admin).deleteIssuer(issuer.address))
+                .to.emit(governance, 'IssuerDeleted')
+                .withArgs(issuer.address);
+
+                const amlResponse = await passport.connect(dataChecker).attributesByDID(did, id("AML"), issuer.address);
+
+                expect(amlResponse.value).equals(hexZeroPad('0x00', 32));
+
+        });
+
+        it("fail - a READER_ROLE gets null attributes if an issuer is disabled", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
+
+            await expect(governance.connect(admin).setIssuerStatus(issuer.address, 1))
+                .to.emit(governance, 'IssuerStatusChanged')
+                .withArgs(issuer.address, 0, 1);
+
+                const amlResponse = await passport.connect(dataChecker).attributesByDID(did, id("AML"), issuer.address);
+
+                expect(amlResponse.value).equals(hexZeroPad('0x00', 32));
+
+        });
+
+        it("fail - a user without READER_ROLE may not query attributes", async () => {
+            await governance.connect(admin).revokeRole(id("READER_ROLE"), dataChecker.address);
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(false);
+
+            await expect(passport.connect(dataChecker).attributesByDID(did, id("AML"), issuer.address)).to.be.revertedWith("INVALID_READER");
+
+        });
+
+        it("fail - cannot access DID level attributes", async () => {
+            expect(await governance.connect(admin).hasRole(id("READER_ROLE"), dataChecker.address)).equals(true);
+
+            const didResponse = await passport.connect(dataChecker).attributesByDID(did, id("DID"), issuer.address);
+            const countryResponse = await passport.connect(dataChecker).attributesByDID(did, id("COUNTRY"), issuer.address);
+            const isBusinessResponse = await passport.connect(dataChecker).attributesByDID(did, id("IS_BUSINESS"), issuer.address);
+
+            expect(didResponse.value).equals(hexZeroPad('0x00', 32));
+            expect(countryResponse.value).equals(hexZeroPad('0x00', 32));
+            expect(isBusinessResponse.value).equals(hexZeroPad('0x00', 32));
+
+        });
+    });
 });
