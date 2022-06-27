a2073e846a3125df09fc9ccbc63f07a50fbae096
teddy-clapp <teddy@quadrata.io>
2022-06-27 13:19:29 -0700

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 27000,
+            runs: 22000,
           },
         }
       },
@@ -42,7 +42,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 27000,
+            runs: 22000,
           },
         }
       }
