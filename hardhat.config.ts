a2b36c772e2a6cabec2737ccbd125ae59ca9dc9e
huy-quadrata <huy@quadrata.io>
2022-07-06 11:12:33 -0700

    fix api key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -62,7 +62,7 @@ const config: HardhatUserConfig = {
     enabled: true,
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   typechain: {
     outDir: "types",
