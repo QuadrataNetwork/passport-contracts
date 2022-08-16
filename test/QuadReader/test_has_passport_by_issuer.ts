d301b7799189d9953f8f95af604cc0e1dbb3a39f
huy-quadrata <huy@quadrata.io>
2022-08-16 14:33:37 -0700

    wip

---
+++ test/QuadReader/test_has_passport_by_issuer.ts
@@ -0,0 +1,83 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Contract } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { formatBytes32String, id } from "ethers/lib/utils";
+
+const {
+  MINT_PRICE,
+  ATTRIBUTE_DID,
+  ATTRIBUTE_AML,
+  ATTRIBUTE_IS_BUSINESS,
+  ATTRIBUTE_COUNTRY,
+  READER_ROLE,
+} = require("../../utils/constant.ts");
+
+const {
+  deployPassportEcosystem,
+} = require("../helpers/deployment_and_init.ts");
+
+const {
+  setAttributes,
+  assertGetAttributes,
+} = require("../helpers/set_attributes.ts");
+
+describe("QuadReader.getAttributes", async () => {
+  let passport: Contract;
+  let governance: Contract; // eslint-disable-line no-unused-vars
+  let reader: Contract; // eslint-disable-line no-unused-vars
+  let defi: Contract; // eslint-disable-line no-unused-vars
+  let deployer: SignerWithAddress, // eslint-disable-line no-unused-vars
+    admin: SignerWithAddress,
+    treasury: SignerWithAddress,
+    minterA: SignerWithAddress,
+    minterB: SignerWithAddress, // eslint-disable-line no-unused-vars
+    issuer: SignerWithAddress,
+    issuer2: SignerWithAddress,
+    issuerTreasury: SignerWithAddress,
+    issuerTreasury2: SignerWithAddress,
+    mockReader: SignerWithAddress;
+
+  let issuedAt: number, verifiedAt: number;
+  const attributes: any = {
+    [ATTRIBUTE_DID]: formatBytes32String("did:quad:123456789abcdefghi"),
+    [ATTRIBUTE_AML]: formatBytes32String("1"),
+    [ATTRIBUTE_COUNTRY]: id("FRANCE"),
+    [ATTRIBUTE_IS_BUSINESS]: id("FALSE"),
+  };
+
+  beforeEach(async () => {
+    [
+      deployer,
+      admin,
+      minterA,
+      minterB,
+      issuer,
+      issuer2,
+      treasury,
+      issuerTreasury,
+      issuerTreasury2,
+      mockReader,
+    ] = await ethers.getSigners();
+    [governance, passport, reader, defi] = await deployPassportEcosystem(
+      admin,
+      [issuer, issuer2],
+      treasury,
+      [issuerTreasury, issuerTreasury2]
+    );
+
+    issuedAt = Math.floor(new Date().getTime() / 1000) - 100;
+    verifiedAt = Math.floor(new Date().getTime() / 1000) - 100;
+
+    await setAttributes(
+      minterA,
+      issuer,
+      passport,
+      attributes,
+      issuedAt,
+      MINT_PRICE
+    );
+
+    await governance.connect(admin).grantRole(READER_ROLE, mockReader.address);
+  });
+});
