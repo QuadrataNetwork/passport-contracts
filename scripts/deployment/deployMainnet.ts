9645f1995bd4ba366e8b323537d4d7e48036ceec
Fabrice Cheng, Quadrata Inc <fabrice@quadrata.io>
2022-08-30 15:54:29 -0700

    add mainnet values

--- scripts/deployment/deployMainnet.ts
+++ scripts/deployment/deployMainnet.ts
@@ -1,24 +1,49 @@
 import { ethers } from "hardhat";
 
 const { deployQuadrata } = require("../../utils/deployment.ts");
+const {
+  ATTRIBUTE_DID,
+  ATTRIBUTE_AML,
+  ATTRIBUTE_COUNTRY,
+  ATTRIBUTE_IS_BUSINESS,
+} = require("../../utils/constant.ts");
 
-const QUADRATA_TREASURY = "";
+const QUADRATA_TREASURY = "0xa011eB50e03CaeCb9b551Df9Df478b6a513e0d21";
 
-const TIMELOCK = "";
+const TIMELOCK = "0x76694A182dB047067521c73161Ebf3Db5Ca988d3";
 
-const MULTISIG = "";
+const MULTISIG = "0xa011eB50e03CaeCb9b551Df9Df478b6a513e0d21";
 
 const TOKEN_IDS = [
   {
     id: 1,
     uri: "ipfs://QmSczMjKWDJBoYSFzPAm3MVFznKcHNnR4EJW23Ng1zQAWu",
   },
+  {
+    id: 2,
+    uri: "ipfs://QmSczMjKWDJBoYSFzPAm3MVFznKcHNnR4EJW23Ng1zQAWu",
+  },
+  {
+    id: 3,
+    uri: "ipfs://QmSczMjKWDJBoYSFzPAm3MVFznKcHNnR4EJW23Ng1zQAWu",
+  },
 ];
 
 // Careful - this doesn't work for Contract Deployment today
-const MAX_GAS_FEE = ethers.utils.parseUnits("4", "gwei");
+const MAX_GAS_FEE = ethers.utils.parseUnits("12.001", "gwei");
 
-const ISSUERS: any[] = []; // {wallet: "0x....", treasury: "0x.....", "attributesPermission": ["kecak256("AML")"]}
+const ISSUERS: any[] = [
+  {
+    wallet: "0x38a08d73153F32DBB2f867338d0BD6E3746E3391", // SpringLabs
+    treasury: "0x5F3f69808772C56Daee7A5d3176990733C67A123", // SpringLabs Issuer
+    attributesPermission: [
+      ATTRIBUTE_DID,
+      ATTRIBUTE_AML,
+      ATTRIBUTE_COUNTRY,
+      ATTRIBUTE_IS_BUSINESS,
+    ],
+  },
+];
 
 (async () => {
   if (!QUADRATA_TREASURY) {
