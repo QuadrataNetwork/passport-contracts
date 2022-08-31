41e7e187dc6bf5d92978c32fee1c3973659da643
Fabrice Cheng, Quadrata Inc <fabrice@quadrata.io>
2022-08-30 17:32:53 -0700

    add final version of migratePassport script

--- scripts/migration/migratePassport.ts
+++ scripts/migration/migratePassport.ts
@@ -1,15 +1,36 @@
 import { ethers } from "hardhat";
 
-const OLD_PASSPORT = "0x911757E425dBc4D5f9E522E87Ab01C8a096AD0D6";
-const NEW_PASSPORT = "0xD3d0fc5387b1F1F210551e7A32eAB1fcF6e9C1a3";
+const OLD_PASSPORT = "0x32791980a332F1283c69660eC8e426de3aD66E7f"; // Mainnet OldQuadPassport.sol
+const NEW_PASSPORT = "";
 
-const ISSUER_ADDRESS = "";
-const MAX_GAS_FEE = ethers.utils.parseUnits("4.1337", "gwei");
+const ISSUER_ADDRESS = "0x38a08d73153F32DBB2f867338d0BD6E3746E3391"; // SpringLabs
+const MAX_GAS_FEE = ethers.utils.parseUnits("5.1337", "gwei");
 const BATCH_SIZE = 10;
 const START_POSITION = 0;
-const END_POSITION = 10;
+const END_POSITION = 0;
 
 (async () => {
+  // // !!!!!!!!!!!!!!! TODO: REMOVE!!!!!!!!!!!!!!!
+  // const impersonatedSigner = await ethers.getImpersonatedSigner(
+  //   "0x76694A182dB047067521c73161Ebf3Db5Ca988d3"
+  // );
+  // console.log(`Loading Impersonating Account: ${impersonatedSigner.address}`);
+  // const [deployer] = await ethers.getSigners();
+  // await deployer.sendTransaction({
+  //   to: impersonatedSigner.address,
+  //   value: ethers.utils.parseEther("1"),
+  // });
+  // const govOld = await ethers.getContractAt(
+  //   "QuadGovernanceOld",
+  //   "0xA16E936425df96b9dA6125B03f19C4d34b315212" // Mainet oldQuadGovernance.sol
+  // );
+  // console.log("Transfer ETH to Timelock");
+  // await govOld
+  //   .connect(impersonatedSigner)
+  //   .grantRole(ethers.utils.id("READER_ROLE"), NEW_PASSPORT);
+  // console.log("Grant READER_ROLE to QuadPassport");
+  // // !!!!!!!!!!!!!!! TODO: END-REMOVE!!!!!!!!!!!!!!!
+
   console.log(`Starting Migration....`);
   console.log(`Max Gas Fee: ${ethers.utils.formatUnits(MAX_GAS_FEE, "gwei")}`);
   const passportOld = await ethers.getContractAt(
@@ -57,14 +78,13 @@ const END_POSITION = 10;
   if (singleBatch.length > 0) {
     batchUsers.push(singleBatch);
   }
-  console.log({ batchUsers });
 
   const [deployer] = await ethers.getSigners();
   const maxPosition = END_POSITION || batchUsers.length;
 
   for (let i = START_POSITION; i < maxPosition; i++) {
     console.log(
-      `---Migrating index ${i} (Max: ${maxPosition}): ${batchUsers[i]}`
+      `---Migrating index ${i}/${maxPosition - 1}: [${batchUsers[i]}]`
     );
     const tx = await passport
       .connect(deployer)
@@ -72,6 +92,6 @@ const END_POSITION = 10;
         maxFeePerGas: MAX_GAS_FEE,
       });
 
-    console.log(`Transaction hash: ${tx.has}`);
+    console.log(`Transaction hash: ${tx.hash}`);
   }
 })();
