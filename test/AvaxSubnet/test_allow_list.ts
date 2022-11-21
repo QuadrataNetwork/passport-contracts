3ef9f0e1ae115c3a7c2150b5b935f83b3c0b3920
teddy-clapp <teddy@quadrata.io>
2022-11-21 12:20:19 -0800

    more passing tests

--- test/AvaxSubnet/test_allow_list.ts
+++ test/AvaxSubnet/test_allow_list.ts
@@ -203,7 +203,7 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.ALLOWED);
     });
 
-    it.only("setAttributes (Multiple issuers for exact same Attribute: AML 1 and then AML 6", async () => {
+    it("setAttributes (Multiple issuers for exact same Attribute: AML 1 and then AML 6", async () => {
       let role = await allowList.readAllowList(minterA.address);
       expect(role).to.be.equal(ROLES.NONE);
       const attributes: any = {
@@ -226,6 +226,7 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.ALLOWED);
 
       const attributes2: any = {
+        [ATTRIBUTE_DID]: formatBytes32String("quad:did:foobar"),
         [ATTRIBUTE_AML]: hexZeroPad("0x06", 32),
       };
       await setAttributes(
@@ -247,7 +248,7 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.NONE);
       const attributes: any = {
         [ATTRIBUTE_DID]: formatBytes32String("quad:did:foobar"),
-        [ATTRIBUTE_AML]: formatBytes32String("6"),
+        [ATTRIBUTE_AML]: hexZeroPad("0x06", 32),
         [ATTRIBUTE_COUNTRY]: id("FRANCE"),
         [ATTRIBUTE_IS_BUSINESS]: id("FALSE"),
       };
@@ -265,7 +266,8 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.NONE);
 
       const attributes2: any = {
-        [ATTRIBUTE_AML]: formatBytes32String("1"),
+        [ATTRIBUTE_DID]: formatBytes32String("quad:did:foobar"),
+        [ATTRIBUTE_AML]: hexZeroPad("0x01", 32),
       };
       await setAttributes(
         minterA,
@@ -286,7 +288,7 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.NONE);
       const attributes: any = {
         [ATTRIBUTE_DID]: formatBytes32String("quad:did:foobar"),
-        [ATTRIBUTE_AML]: formatBytes32String("6"),
+        [ATTRIBUTE_AML]: hexZeroPad("0x06", 32),
         [ATTRIBUTE_COUNTRY]: id("FRANCE"),
         [ATTRIBUTE_IS_BUSINESS]: id("FALSE"),
       };
@@ -304,7 +306,8 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.NONE);
 
       const attributes2: any = {
-        [ATTRIBUTE_AML]: formatBytes32String("1"),
+        [ATTRIBUTE_DID]: formatBytes32String("quad:did:foobar"),
+        [ATTRIBUTE_AML]: hexZeroPad("0x01", 32),
       };
       await setAttributes(
         minterA,
@@ -320,12 +323,12 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.ALLOWED);
     });
 
-    it("setAttributes (single issuer override AML 1 and then AML 6", async () => {
+    it.only("setAttributes (single issuer override AML 1 and then AML 6", async () => {
       let role = await allowList.readAllowList(minterA.address);
       expect(role).to.be.equal(ROLES.NONE);
       const attributes: any = {
         [ATTRIBUTE_DID]: formatBytes32String("quad:did:foobar"),
-        [ATTRIBUTE_AML]: formatBytes32String("1"),
+        [ATTRIBUTE_AML]: hexZeroPad("0x01", 32),
         [ATTRIBUTE_COUNTRY]: id("FRANCE"),
         [ATTRIBUTE_IS_BUSINESS]: id("FALSE"),
       };
@@ -343,7 +346,8 @@ describe("AllowList", function() {
       expect(role).to.be.equal(ROLES.ALLOWED);
 
       const attributes2: any = {
-        [ATTRIBUTE_AML]: formatBytes32String("6"),
+        [ATTRIBUTE_DID]: formatBytes32String("quad:did:foobar"),
+        [ATTRIBUTE_AML]: hexZeroPad("0x06", 32),
       };
       await setAttributes(
         minterA,
