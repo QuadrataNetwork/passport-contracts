2536a46f672ec545017ba4880998e4b44ba685ba
teddy-clapp <teddy@quadrata.io>
2022-11-23 15:02:08 -0800

    add credscore to testnet

--- utils/constant.ts
+++ utils/constant.ts
@@ -17,6 +17,7 @@ export const ATTRIBUTE_AML = ethers.utils.id("AML");
 export const ATTRIBUTE_COUNTRY = ethers.utils.id("COUNTRY");
 export const ATTRIBUTE_DID = ethers.utils.id("DID");
 export const ATTRIBUTE_IS_BUSINESS = ethers.utils.id("IS_BUSINESS");
+export const ATTRIBUTE_CRED_PROTOCOL_SCORE = ethers.utils.id("CRED_PROTOCOL_SCORE")
 export const TOKEN_ID = 1;
 
 export const ALL_ROLES = [
@@ -59,6 +60,7 @@ export const PRICE_PER_ATTRIBUTES_ETH = {
   [ATTRIBUTE_COUNTRY]: parseEther("0.0012"),
   [ATTRIBUTE_DID]: parseEther("0.0012"),
   [ATTRIBUTE_IS_BUSINESS]: parseEther("0"),
+  [ATTRIBUTE_CRED_PROTOCOL_SCORE]: parseEther("0.012"),
 };
 
 export const PRICE_PER_BUSINESS_ATTRIBUTES_ETH = {
@@ -66,6 +68,8 @@ export const PRICE_PER_BUSINESS_ATTRIBUTES_ETH = {
   [ATTRIBUTE_COUNTRY]: parseEther("0.015"),
   [ATTRIBUTE_DID]: parseEther("0.015"),
   [ATTRIBUTE_IS_BUSINESS]: parseEther("0"),
+  [ATTRIBUTE_CRED_PROTOCOL_SCORE]: parseEther("0.015"),
+
 };
 
 export const PRICE_PER_ATTRIBUTES_MATIC = {
@@ -73,6 +77,7 @@ export const PRICE_PER_ATTRIBUTES_MATIC = {
   [ATTRIBUTE_COUNTRY]: parseEther("2"),
   [ATTRIBUTE_DID]: parseEther("2"),
   [ATTRIBUTE_IS_BUSINESS]: parseEther("0"),
+  [ATTRIBUTE_CRED_PROTOCOL_SCORE]: parseEther(".5"),
 };
 
 export const PRICE_PER_BUSINESS_ATTRIBUTES_MATIC = {
@@ -80,6 +85,7 @@ export const PRICE_PER_BUSINESS_ATTRIBUTES_MATIC = {
   [ATTRIBUTE_COUNTRY]: parseEther("25"),
   [ATTRIBUTE_DID]: parseEther("25"),
   [ATTRIBUTE_IS_BUSINESS]: parseEther("0"),
+  [ATTRIBUTE_CRED_PROTOCOL_SCORE]: parseEther("6"),
 };
 
 export const PRICE_PER_ATTRIBUTES = {
