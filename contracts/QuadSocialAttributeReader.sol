39065dbba37e041d16190c58db7cd81d8f2cbd40
huy-quadrata <huy@quadrata.io>
2022-12-14 14:38:44 -0800

    WIP

--- contracts/QuadSocialAttributeReader.sol
+++ contracts/QuadSocialAttributeReader.sol
@@ -1,15 +1,17 @@
-//SPDX-License-Identifier: BUSL-1.1
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import "./IQuadReader.sol";
+import "../interfaces/IQuadReader.sol";
+import "../interfaces/IQuadGovernance.sol";
 import "../interfaces/IQuadPassportStore.sol";
 
-contract QuadSocialAttributeReader {
+contract SocialAttributeReader {
     using SafeMath for uint256;
 
     address public constant QUAD_READER = 0x5C6b81212c0A654B6e247F8DEfeC9a95c63EF954;
+    address public constant QUAD_GOVERNANCE = 0x863db2c1A43441bbAB7f34740d0d62e21e678A4b;
 
     // keccak256(userAddress, issuerAddr, attrType))
     mapping(bytes32 => IQuadPassportStore.Attribute[]) internal _attributes;
@@ -17,12 +19,11 @@ contract QuadSocialAttributeReader {
     mapping(address=>uint256) public issuerQueryFee;
     mapping(address=>uint256) public funds;
 
-    address public quadrataAdmin;
 
     function writeAttributes(bytes32 _attrName, bytes32 _attrValue, address _targetAddr) public {
         require(allowList[_targetAddr][msg.sender], 'NOT_ALLOWED');
-        bytes32 attrKey = keccak256(abi.encode(_targetAddr, msg.sender, _attrName)); 
-        
+        bytes32 attrKey = keccak256(abi.encode(_targetAddr, msg.sender, _attrName));
+
         IQuadPassportStore.Attribute memory attr = IQuadPassportStore.Attribute({
             value:  _attrValue,
             epoch: block.timestamp,
@@ -33,12 +34,13 @@ contract QuadSocialAttributeReader {
     }
 
     function isSocialAttribute(bytes32 _attrName) internal returns(bool) {
-        return true;
+        IQuadGovernance governance = IQuadGovernance(QUAD_GOVERNANCE);
+        return !governance.eligibleAttributes(_attrName) && !governance.eligibleAttributesByDID(_attrName);
     }
-  
+
     function queryFee(
-        address _issuer, 
-        address _account, 
+        address _issuer,
+        address _account,
         bytes32[] calldata _attrNames) public returns(uint256){
         uint256 fee;
         for(uint256 i = 0; i < _attrNames.length; i++){
@@ -52,10 +54,10 @@ contract QuadSocialAttributeReader {
 
         return fee;
     }
-  
+
     function getAttributesBulk(
-        address _issuer, 
-        address _account, 
+        address _issuer,
+        address _account,
         bytes32[] calldata _attrNames
     ) payable public returns(IQuadPassportStore.Attribute[] memory){
         uint256 quadrataFee;
@@ -77,37 +79,41 @@ contract QuadSocialAttributeReader {
             }
         }
         require(msg.value == (quadrataFee + issuerFee), "INVALID_FEE");
-        
-        funds[quadrataAdmin] += quadrataFee;
+
+        funds[quadrataTreasury()] += quadrataFee;
         funds[_issuer] += issuerFee;
-        
+
         return attributes;
+    }
+
+    function quadrataTreasury() view internal returns (address) {
+        return IQuadGovernance(QUAD_GOVERNANCE).treasury();
     }
 
     function calculateSocialFees(address _issuer) view internal returns (uint256, uint256){
         uint256 quadrataFee;
         uint256 issuerFee;
-        if(issuerQueryFee[_issuer].div(uint256(2)) > issuerQueryFee[quadrataAdmin]) {
+        if(issuerQueryFee[_issuer].div(uint256(2)) > issuerQueryFee[quadrataTreasury()]) {
             issuerFee = issuerQueryFee[_issuer].div(uint256(2));
             quadrataFee = issuerQueryFee[_issuer] - issuerFee;
         }else{
             issuerFee = issuerQueryFee[_issuer].div(uint256(2));
-            quadrataFee = issuerQueryFee[quadrataAdmin];
+            quadrataFee = issuerQueryFee[quadrataTreasury()];
         }
         return(issuerFee, quadrataFee);
     }
-  
+
     function allowAddress(address _addr, bool _allow) public {
         allowList[msg.sender][_addr] = _allow;
     }
-  
+
     function withdraw() public {
         require(funds[msg.sender] > 0, "CANNOT_WITHDRAW");
         uint256 amount = funds[msg.sender];
         funds[msg.sender] = 0;
         payable(msg.sender).transfer(amount);
     }
-    
+
     function setIssuerQueryFee(uint256 _amount) public {
         issuerQueryFee[msg.sender] = _amount;
     }
