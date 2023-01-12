77fd72b6c2f3c4c83537b4baa3570f987d8b8461
huy-quadrata <huy@quadrata.io>
2023-01-12 12:34:57 -0800

    WIP

--- scripts/deployment/flexkit/deployTestnet.ts
+++ scripts/deployment/flexkit/deployTestnet.ts
@@ -3,10 +3,10 @@ import { ethers } from "hardhat";
 const { deployFlexkit } = require("../../utils/deployment.ts");
 
 (async () => {
-  const governanceAddress = '';
-  const readerAddress = '';
+  // Integration Mumbai/Goerli Addresses
+  const governanceAddress = '0x863db2c1A43441bbAB7f34740d0d62e21e678A4b';
+  const readerAddress = '0x5C6b81212c0A654B6e247F8DEfeC9a95c63EF954';
 
   const flexkit = await deployFlexkit(governanceAddress, readerAddress);
   console.log(`Flexkit address: ${flexkit.address}`)
-
 })();
