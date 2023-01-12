4f6baa657b720e70bffef5b9160f63c6e6196ee3
huy-quadrata <huy@quadrata.io>
2023-01-12 12:31:37 -0800

    wip

--- scripts/deployment/flexkit/deployTestnet.ts
+++ scripts/deployment/flexkit/deployTestnet.ts
@@ -1,44 +1,12 @@
 import { ethers } from "hardhat";
 
-const { deployQuadrata } = require("../../utils/deployment.ts");
-
-// const {
-//   GOVERNANCE_ROLE,
-//   DEFAULT_ADMIN_ROLE,
-// } = require("../../utils/constant.ts");
-
-// const {
-//   QUADRATA_TREASURY,
-//   TIMELOCK,
-//   MULTISIG,
-//   TOKEN_IDS,
-//   ISSUERS,
-//   MAX_GAS_FEE,
-// } = require("../data/testnet.ts");
+const { deployFlexkit } = require("../../utils/deployment.ts");
 
 (async () => {
-  console.log(
-    `Set maxFeePerGas to ${ethers.utils.formatUnits(MAX_GAS_FEE, "gwei")} Gwei`
-  );
+  const governanceAddress = '';
+  const readerAddress = '';
 
-  // Retrieve address filter by Network
-  const signers: any = await ethers.getSigners();
-  const network = await signers[0].provider.getNetwork();
-  const treasuryPerNetwork = QUADRATA_TREASURY[network.chainId];
-  const multisigPerNetwork = MULTISIG[network.chainId];
-
-  const deployer = signers[0];
-
-  console.log(`Deployer address: ${deployer.address}`);
-
-  const [governance] = await deployQuadrata(
-    TIMELOCK,
-    ISSUERS,
-    treasuryPerNetwork,
-    multisigPerNetwork,
-    TOKEN_IDS,
-    true, // Verbose = true,
-    MAX_GAS_FEE
-  );
+  const flexkit = await deployFlexkit(governanceAddress, readerAddress);
+  console.log(`Flexkit address: ${flexkit.address}`)
 
 })();
