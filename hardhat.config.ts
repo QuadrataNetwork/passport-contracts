cf088621fe0e32e79b5eabf2ce22dbd4085f298e
teddy-clapp <teddy@quadrata.io>
2023-02-02 17:29:40 -0800

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -182,7 +182,7 @@ task("addIssuer", "Example: npx hardhat addIssuer --issuer 0x696969696943441bbAB
         await governance.addIssuer(issuerAddress, issuerAddress);
         console.log("Issuer Added");
       } catch (e) {
-        console.log(e);
+        console.log("Error Adding Issuer");
         console.log("retrying in 5ish seconds");
         setTimeout(addIssuerRetry, 4000 + Math.random() * 2000);
       }
@@ -211,9 +211,11 @@ task("addIssuers", "Example: npx hardhat addIssuers --issuers 0x696969696943441b
           await governance.addIssuer(issuerAddress, issuerAddress);
           console.log("added " + issuerAddress)
         } catch (e) {
-          console.log("failed to add" + e);
+          console.log("failed to add" + issuerAddress);
           console.log("retrying in 5ish seconds");
-          setTimeout(addIssuerRetry, 4000 + Math.random() * 2000);
+          let wait = (ms: any) => new Promise(resolve => setTimeout(resolve, ms));
+          await wait(4000 + Math.random() * 2000);
+          await addIssuerRetry();
         }
       }
 
@@ -250,4 +252,10 @@ task("getIssuers", "Example: npx hardhat getIssuers --governance 0x863db2c1A4344
   npx hardhat getIssuers --governance 0x82F5a215f29089429C634d686103D297b85d4e2a --network optimism_goerli
   npx hardhat getIssuers --governance 0x82F5a215f29089429C634d686103D297b85d4e2a --network fantom_testnet
 
+  */
+
+  /*
+
+  // usage for adding all issuers for all testnets
+  npx hardhat addIssuers --issuers 0x0706a7CFC2d1B8EcFbF3dF103095Ac3047BeA431,0xd0c0256D625a74c0ACb7447Bd0940f23BBe33ff1,0x3097988FD29cD00f2C27B2b964F99Ac974d30A41,0x1135F3b9f2895Fb1B688B354Aea3C31114B49a38,0x8859c986F102924DBeC3767b67497b8d89Be2463 --governance 0x863db2c1A43441bbAB7f34740d0d62e21e678A4b --network goerli
   */
