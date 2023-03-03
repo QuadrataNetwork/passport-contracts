17e38bc0eebaa21e62c5beeb60b273c34f849709
teddy-clapp <teddy@quadrata.io>
2023-03-03 10:05:10 -0700

    working polygon test

--- test/DryRuns/test_migrate_to_v3_storage.ts
+++ test/DryRuns/test_migrate_to_v3_storage.ts
@@ -6,7 +6,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-wit
 import { defaultAbiCoder, hexZeroPad, parseUnits } from "ethers/lib/utils";
 import { ATTRIBUTE_AML, ATTRIBUTE_COUNTRY, ATTRIBUTE_CRED_PROTOCOL_SCORE, ATTRIBUTE_DID, ATTRIBUTE_IS_BUSINESS } from "../../utils/constant";
 
-describe.only('migrateToV3Storage', function() {
+describe.skip('migrateToV3Storage', function() {
     // increase timeout to 600s
     this.timeout(600000);
 
